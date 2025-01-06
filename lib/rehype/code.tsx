6a439f1f6dee93abc2eea26fd6cc06eeeb350b9d
Raphael Tang <raphpb1912@gmail.com>
2025-01-07 05:19:18 +0800

    feat: improved copy button

--- lib/rehype/code.tsx
+++ lib/rehype/code.tsx
@@ -5,8 +5,8 @@ import {
   SpatialMaterial,
 } from "@/components/spatial/material";
 import { ClientOnly } from "@/components/utils/client-only";
-import { CopyIcon } from "lucide-react";
-import { ComponentPropsWithoutRef, useEffect, useRef } from "react";
+import { CopyCheckIcon, CopyIcon } from "lucide-react";
+import { ComponentPropsWithoutRef, useEffect, useRef, useState } from "react";
 import { cn } from "../utils";
 
 export type CodeBlockProps = ComponentPropsWithoutRef<"div">;
@@ -21,6 +21,7 @@ export const CodeBlock = (props: CodeBlockProps) => {
 
 const Inner = ({ children, ...props }: CodeBlockProps) => {
   const ref = useRef<HTMLDivElement | null>(null);
+  const [copied, setCopied] = useState(false);
 
   useEffect(() => {
     if (ref.current) {
@@ -28,8 +29,12 @@ const Inner = ({ children, ...props }: CodeBlockProps) => {
         "figcaption[data-rehype-pretty-code-title]",
       );
       element.style.margin = "0";
-      element.style.flex = "1";
       element.style.display = "flex";
+      element.style.position = "absolute";
+      element.style.top = "0";
+      element.style.left = "0";
+      element.style.width = "100%";
+      element.style.height = "100%";
       element.style.alignItems = "center";
       element.style.justifyContent = "center";
       ref.current
@@ -49,12 +54,18 @@ const Inner = ({ children, ...props }: CodeBlockProps) => {
         borderColor: "#343434",
       }}
     >
-      <div className="flex flex-row px-2 py-2" data-rehype-pretty-code-header>
+      <div
+        className="flex flex-row px-2 py-2 relative"
+        data-rehype-pretty-code-header
+      >
         <SpatialMaterial
-          className="flex flex-row gap-2 text-xs items-center justify-center rounded-md select-none"
+          className={cn(
+            "flex flex-row gap-2 rounded-md px-4 py-2",
+            "items-center justify-center",
+            "text-xs select-none",
+          )}
           style={{
             boxShadow: "none",
-            padding: 8,
           }}
           whileHover={{
             boxShadow: MATERIAL_BORDER,
@@ -65,9 +76,11 @@ const Inner = ({ children, ...props }: CodeBlockProps) => {
               .querySelector("div[data-shiki]")
               .getAttribute("data");
             navigator.clipboard.writeText(sourceCode);
+            setCopied(true);
+            setTimeout(() => setCopied(false), 1 * 1000);
           }}
         >
-          <CopyIcon size={16} />
+          {copied ? <CopyCheckIcon size={14} /> : <CopyIcon size={14} />}
           <span>Copy</span>
         </SpatialMaterial>
       </div>
