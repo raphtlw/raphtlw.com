63346f8d8ea8c52dc5d921f0e8a43770456b6daa
Raphael Tang <raphpb1912@gmail.com>
2025-01-07 04:49:46 +0800

    fix(build): another one hiding behind the shadows...

--- components/misc/mdx.tsx
+++ components/misc/mdx.tsx
@@ -1,8 +1,5 @@
 import { cn } from "@/lib/utils";
-import { sanityFetch } from "@/sanity/lib/live";
-import { QUERY_AUTHOR } from "@/sanity/lib/queries";
-import { ComponentProps, PropsWithChildren } from "react";
-import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
+import { PropsWithChildren } from "react";
 
 export type LayoutProps = PropsWithChildren;
 
@@ -17,45 +14,5 @@ export function Layout({ children }: LayoutProps) {
         {children}
       </article>
     </main>
-  );
-}
-
-export type BlogHeaderProps = ComponentProps<"header"> & {
-  title: string;
-  authorSlug: string;
-};
-
-export async function BlogHeader({
-  title,
-  authorSlug,
-  ...props
-}: BlogHeaderProps) {
-  const { data: author } = await sanityFetch({
-    query: QUERY_AUTHOR,
-    params: {
-      slug: authorSlug,
-    },
-  });
-
-  return (
-    <header
-      {...props}
-      className="mt-10 flex flex-col gap-4 md:max-w-3xl mx-auto"
-    >
-      <h1 className="text-3xl font-bold tracking-tight">{title}</h1>
-
-      <div className="flex flex-row items-center gap-4">
-        <Avatar>
-          <AvatarImage src={author.imageUrl} />
-          <AvatarFallback>
-            {authorSlug.substring(0, 2).toUpperCase()}
-          </AvatarFallback>
-        </Avatar>
-        <div className="flex flex-col">
-          <p className="text-sm text-slate-200 font-medium">{author.name}</p>
-          <p className="text-sm text-accent">@{authorSlug}</p>
-        </div>
-      </div>
-    </header>
   );
 }
